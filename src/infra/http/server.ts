c9890748e8027a86388f8805e5b55d3d00337605
Pedro Ferrauche <pedro.ferrauche@hotmail.com>
2025-10-15 22:39:07 -0300

    chore: configuração do Vitest para testes e Swagger para documentação da API.

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,9 +1,47 @@
 import { fastifyCors } from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.validation,
+    })
+  }
+
+  console.error(error)
+
+  return reply.status(500).send({ message: 'Internal server error' })
+})
+
 server.register(fastifyCors, { origin: '*' })
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Url Shortener',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
