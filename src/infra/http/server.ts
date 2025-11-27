dbb33f334ab797616293da7003699bb312afd4dd
Pedro Ferrauche <pedro.ferrauche@hotmail.com>
2025-11-27 00:26:05 -0300

    fix: declaração dos métodos no registro do fastifyCors em server.ts

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -8,12 +8,12 @@ import {
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
-import { createLinkRoute } from './routes/create-link'
-import { deleteLinkRoute } from './routes/delete-link'
-import { exportLinksRoute } from './routes/export-links'
-import { getLinksRoute } from './routes/get-links'
-import { getOriginalUrlLinkRoute } from './routes/get-original-url-link'
-import { incrementClickLinkRoute } from './routes/increment-click-link'
+import { createLinkRoute } from '@/infra/http/routes/create-link'
+import { deleteLinkRoute } from '@/infra/http/routes/delete-link'
+import { exportLinksRoute } from '@/infra/http/routes/export-links'
+import { getLinksRoute } from '@/infra/http/routes/get-links'
+import { getOriginalUrlLinkRoute } from '@/infra/http/routes/get-original-url-link'
+import { incrementClickLinkRoute } from '@/infra/http/routes/increment-click-link'
 
 const server = fastify()
 
@@ -28,12 +28,13 @@ server.setErrorHandler((error, request, reply) => {
     })
   }
 
-  console.error(error)
-
   return reply.status(500).send({ message: 'Internal server error' })
 })
 
-server.register(fastifyCors, { origin: '*' })
+server.register(fastifyCors, {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+})
 
 server.register(fastifySwagger, {
   openapi: {
